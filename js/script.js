6da6a2668bfef2b55948af4c6022daf939b19153
YuliiaPol <julia.polyeshko@gmail.com>
2020-05-07 12:26:43 +0300

    script registration

--- js/script.js
+++ js/script.js
@@ -2,15 +2,14 @@ $(document).ready(function () {
     PopUpHide();
     $(".form-registretion").submit(function (event) {
         PopUpShow();
-        event.preventDefault();
     });
     $(".close-popup").click(function() {
         PopUpHide();
     });
-    function PopUpShow() {
-        $(".succes-box").show();
-    }
-    function PopUpHide() {
-        $(".succes-box").hide();
-    }
 });
+function PopUpShow() {
+    $(".succes-box").show();
+}
+function PopUpHide() {
+    $(".succes-box").hide();
+}
